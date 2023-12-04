f0174b6577a98bd500e36752582d233890bc174b
Seb59001 <leg.sebastien@gmail.com>
2023-12-04 12:08:41 +0100

    TP3 terminé

--- cours/Javascript/TP/TP3/js/script.js
+++ cours/Javascript/TP/TP3/js/script.js
@@ -1,27 +1,57 @@
 
 // Écouteur d'évènement sur le premier rectangle
 
+let rectangle1 = document.querySelector('#click');
+
+rectangle1.addEventListener('click', function() {
+    this.classList.toggle('yellow');
+
+    // this.style.backgroundColor = 'red';
+});
 
 
 // Écouteur d'évènement sur le deuxième rectangle
 
 
+let rectangle2 = document.querySelector('#double-click');
+
+rectangle2.addEventListener('dblclick', function() {
+    this.classList.toggle('rotate');
+});
+
+
 
 // Écouteurs d'évènements sur le troisième rectangle
 
+let rectangle3 = document.querySelector('#mouse');
 
-
-
-
-
+rectangle3.addEventListener('mouseenter', function() {
+    this.classList.toggle('bigger');
+})
 
 
 
 // Écouteur d'évènement sur le 4ème rectangle
 
+let rectangle4 = document.querySelector('#copy');
 
+rectangle4.addEventListener('copy', function() {
+    this.textContent = "......";
+})
 
 // Écouteur d'évènement sur le 5ème rectangle
+
+
+let rectangle5 = document.querySelector('#live');
+
+let input = document.querySelector('div>input');
+
+let p = document.querySelector('div>p')
+
+
+rectangle5.addEventListener('keyup', function () {
+    p.textContent = input.value;
+});
 
 
 

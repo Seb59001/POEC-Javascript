8bf0d5605613af56f0fe34cd97dfbca9d2c71545
Seb59001 <leg.sebastien@gmail.com>
2023-12-08 15:17:16 +0100

    TP theme sombre

--- cours/Javascript/TP/Tp - theme sombre/Base/js/script.js
+++ cours/Javascript/TP/Tp - theme sombre/Base/js/script.js
@@ -3,8 +3,27 @@
 // fonction qui permet de mettre en place ou supprimer la classe qui changera le thème de couleur de notre page
 
 
+
+// Fonction qui permet de mettre en place ou supprimer la classe qui changera le thème de couleur de notre page
+function theme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-theme');
+    } else if (theme === 'light') {
+        document.body.classList.remove('dark-theme');
+    }
+}
+
 // Écouteur d'évènement pour attendre le chargement complet du DOM
 document.addEventListener('DOMContentLoaded', function(){
-    // écoute des deux boutons au clic
+    // Écoute des deux boutons au clic
+    let lightBtn = document.querySelector('#theme-light');
+    let darkBtn = document.querySelector('#theme-dark');
 
+    darkBtn.addEventListener('click', function() {
+        theme('dark');
+    });
+
+    lightBtn.addEventListener('click', function() {
+        theme('light');
+    });
 });
